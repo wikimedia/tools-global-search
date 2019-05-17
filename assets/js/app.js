3b0b738eb2a866c94fcb0275e4ab940b4074ea98
MusikAnimal <musikanimal@gmail.com>
2019-05-17 17:30:50 +0200

    Add dependencies that we'll probably need

---
+++ assets/js/app.js
@@ -0,0 +1,4 @@
+require('../css/app.css');
+const $ = require('jquery');
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,9 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Wikimedia\ToolforgeBundle\ToolforgeBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
