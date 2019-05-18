6fe9ab3309312a3c0f043dc330d615fc8016ea5e
MusikAnimal <musikanimal@gmail.com>
2019-05-18 17:58:25 +0200

    Faster regex search

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -19,6 +19,9 @@ class DefaultController extends AbstractController
     /** @var CacheItemPoolInterface */
     private $cache;
 
+    private const PRE_TAG = '%**%';
+    private const POST_TAG = '*%%*';
+
     /**
      * Index page with the form to enter a search query.
      * @Route("/")
@@ -47,9 +50,74 @@ class DefaultController extends AbstractController
         $this->client = new Client([
             'verify' => $_ENV['ELASTIC_INSECURE'] ? false : true
         ]);
-        $uri = $_ENV['ELASTIC_HOST'].'/*,*:*/_search?q='.urlencode($query);
+        $uri = $_ENV['ELASTIC_HOST'].'/*,*:*/_search?preference=_prefer_nodes:cloudelastic1001-cloudelastic-chi-eqiad,cloudelastic1002-cloudelastic-chi-eqiad,cloudelastic1003-cloudelastic-chi-eqiad';
+//        $uri = $_ENV['ELASTIC_HOST'].'/psi:testwiki/_search?preference=_prefer_nodes:cloudelastic1001-cloudelastic-chi-eqiad,cloudelastic1002-cloudelastic-chi-eqiad,cloudelastic1003-cloudelastic-chi-eqiad';
 
-        $res = $this->client->request('GET', $uri);
+        $filters = [
+//            {'term': {'namespace': str(args.ns)}},
+            'source_regex' => [
+                'regex' => $query,
+                'field' => 'source_text',
+                'ngram_field' => 'source_text.trigram',
+                'max_determinized_states' => 20000,
+                'max_expand' => 10,
+                'case_sensitive' => true,
+                'locale' => 'en',
+            ],
+        ];
+
+        $request = new \GuzzleHttp\Psr7\Request('GET', $uri, [
+            'Content-Type' => 'application/json',
+        ], \GuzzleHttp\json_encode([
+            'timeout' => '150s',
+            'size' => 100,
+            '_source' => ['wiki', 'namespace_text', 'title'],
+            'query' => [
+                'bool' => [
+                    'filter' => $filters,
+                ]
+            ],
+            'highlight' => [
+                'pre_tags' => [self::PRE_TAG],
+                'post_tags' => [self::POST_TAG],
+                'fields' => [
+                    'source_text.plain' => [
+                        'type' => 'experimental',
+                        'number_of_fragments' => 1,
+                        'fragmenter' => 'scan',
+                        'fragment_size' => 150,
+                        'options' => [
+                            'regex' => [$query],
+                            'locale' => 'en',
+                            'regex_flavor' => 'lucene',
+                            'skip_query' => true,
+                            'max_determinized_states' => 20000,
+                        ]
+//                        'highlight_query' => [
+//                            'bool' =>
+//                        ],
+                    ],
+                ],
+            ],
+            'stats' => ['global-search'],
+        ]));
+        $res = $this->client->send($request);
+//        $res = $this->client->request('GET', $uri, [
+//            'form_params' => [
+//                'size' => 10,
+//                '_source' => ['wiki', 'namespace_text', 'title'],
+//                'query' => [
+//                    'bool' => [
+//                        'filters' => $filters,
+//                    ]
+//                ],
+//                'stats' => ['global-search'],
+//            ],
+//            'headers' => [
+//                'Content-Type' => 'application/json',
+//            ],
+////            'http_errors' => false,
+//        ]);
         $data = json_decode($res->getBody()->getContents(), true);
         $data = [
             'total' => $data['hits']['total'],
@@ -71,12 +139,13 @@ class DefaultController extends AbstractController
 
         foreach ($hits as $hit) {
             $result = $hit['_source'];
+            $title = ($result['namespace_text'] ? $result['namespace_text'].':' : '').$result['title'];
             $newData[] = [
                 'wiki' => rtrim($this->getWikiDomainFromDbName($result['wiki']), '.org'),
-                'title' => $result['title'],
-                'url' => $this->getUrlForTitle($result['wiki'], $result['title']),
-                'text' => $this->highlightQueryInText($result['text'], $query),
-                'source_text' => $this->highlightQueryInText($result['source_text'], $query),
+                'title' => $title,
+                'url' => $this->getUrlForTitle($result['wiki'], $title),
+                'source_text' => $this->highlightQuery($hit['highlight']['source_text.plain'][0]),
+//                'source_text' => $this->highlightQuery($result['source_text'], $query),
             ];
         }
 
@@ -106,12 +175,20 @@ class DefaultController extends AbstractController
         return $domain;
     }
 
-    private function highlightQueryInText(string $text, string $query): string
+    private function highlightQuery(string $text): string
     {
         // First make the original text HTML safe.
         $text = htmlspecialchars($text);
 
-        // Highlight the query, and return the result.
-        return preg_replace('/'.$query.'/i', "<span class='text-danger'>$query</span>", $text);
+        return strtr($text, [
+            self::PRE_TAG => "<span class='highlight'>",
+            self::POST_TAG => "</span>",
+        ]);
+//        return preg_replace('/'.$query.'/i', "<span class='text-danger'>$query</span>", $text);
     }
+
+//    private function truncateText(string $text, string $query): string
+//    {
+//
+//    }
 }
